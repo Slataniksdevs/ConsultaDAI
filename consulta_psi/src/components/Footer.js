a5acc4e240e3f9e3d79c6827c63845fc875ba54e
Slatanik <manuel.marti29@gmail.com>
2024-03-27 13:55:26 -0300

    tercer commit se crea un componente

---
+++ consulta_psi/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Este es el pie de página de mi aplicación.</p>
+    </footer>
+  );
+};
+
+export default Footer;
