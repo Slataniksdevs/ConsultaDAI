f580e881cf2ee6877cd92f064dff806e64ed7ada
Slatanik <manuel.marti29@gmail.com>
2024-03-29 18:09:57 -0300

    primer levantamiento

--- Frontend/consulta_dai/src/App.js
+++ Frontend/consulta_dai/src/App.js
@@ -1,14 +1,8 @@
 import React from 'react';
-<<<<<<< HEAD
-import Navbar from './components/Navbar'; // Cambiar la ruta de importación a relativa
-import Login from './components/login';
-import { ChakraProvider } from '@chakra-ui/react';
-=======
 import Navbar from './components/Navbar/Navbar'; 
-import './index.css'; // Importa tus estilos globales si los tienes
-import 'tailwindcss/tailwind.css'; // Importa Tailwind CSS
+import Login from './components/login';
+import { ChakraProvider } from '@chakra-ui/react'
 
->>>>>>> 733bea16ec1472f0d752b388af214bdacd05ef5d
 
 function App() {
   return (
