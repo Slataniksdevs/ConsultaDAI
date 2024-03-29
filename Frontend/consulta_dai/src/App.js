c29ad3466c7c2fb77ff87a832f45f23c1ac2b564
Slatanik <manuel.marti29@gmail.com>
2024-03-29 17:43:46 -0300

    integración de chakraui para interfaz front

--- Frontend/consulta_dai/src/App.js
+++ Frontend/consulta_dai/src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Navbar from './components/Navbar'; // Cambiar la ruta de importación a relativa
+import Login from './components/login';
+import { ChakraProvider } from '@chakra-ui/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Navbar />
-      </header>
-    </div>
+<ChakraProvider>
+      <div className="App">
+        <header className="App-header">
+          <Navbar />
+        </header>
+      <Login />
+      </div>
+
+</ChakraProvider>
   );
 }
 
