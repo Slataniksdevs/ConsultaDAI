5aed30f7a5cfce6b884ce0dd03f997b54d114817
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-03-29 23:58:11 -0300

    start users endpoint

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -1,15 +1,2 @@
-import psycopg2
-
-#Configuracion a la base de datos
-
-DB_CONFIG = {
-    'dbname' : 'consulta_dai_dev',
-    'user' : 'postgres',
-    'password' : 'Postgres$37116',
-    'host' : 'tmp.enred.cl'
-}
-
-
-def connect():
-    conn =  psycopg2.connect(**DB_CONFIG)
-    return conn
+from flask import Flask, jsonify, request, Blueprint
+from 'database/config' import connect
