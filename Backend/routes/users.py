859a8a25588276da90b3c71b0458d4ff668b29ad
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-04-01 17:09:15 -0300

    post, pass bcryp done

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -1,9 +1,49 @@
 from flask import Flask,jsonify,request,Blueprint
 from config import connect
 from flask_cors import CORS
+from flask_bcrypt import generate_password_hash
 
 usuarios_bp = Blueprint('usuario', __name__)
 
+#POST PARA CREAR USUARIO CON LA PASSWORD ENCRIPTADA
+@usuarios_bp.route('/usuarios/register', methods=['POST'])
+def register():
+    try:
+        data = request.json
+        username = data['username']
+        email = data['email']
+        password = data['password']
+        first_name = data['first_name']
+        last_name  = data['last_name']
+        phone  = data['phone']
+        birthdate  = data['birthdate']
+        gender  = data['gender']
+        country  = data['country']
+        
+        #encriptar la password
+        hashed_password = generate_password_hash(password)
+        
+        conn = connect()
+        cursor = conn.cursor()
+        cursor.execute("""
+            INSERT INTO public.users(
+                username, email, password, first_name, last_name,
+                phone, birthdate, gender, country
+            )
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """, (
+            username, email, hashed_password, first_name, last_name,
+            phone, birthdate, gender, country
+        ))
+        conn.commit()
+        conn.close()
+    
+        return jsonify({'message': 'Usuario registrado correctamente'}), 201
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+        
+
+#GET PARA OBTENER USUARIOS (LISTA)
 @usuarios_bp.route('/usuarios/get_user', methods = ['GET'])
 def get_users():
     conn = connect()
@@ -30,4 +70,3 @@ def get_users():
         
         
         
-        # username, email, password, first_name, last_name, phone, birthdate, gender, country
