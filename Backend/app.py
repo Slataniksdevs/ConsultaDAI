1de8f6ceea56dcb73b5024ece55b237fc207a687
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-04-01 10:47:36 -0300

    get user check

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,13 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+from config import connect
 from flask_cors import CORS
-from routes import users_bp
+from routes.users import usuarios_bp
 
 app = Flask(__name__)
 CORS(app)
 
-app.register_blueprint(users_bp)
+
+app.register_blueprint(usuarios_bp)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -1,35 +1,33 @@
-from flask import jsonify, request, Blueprint
+from flask import Flask,jsonify,request,Blueprint
 from config import connect
 from flask_cors import CORS
 
-users_bp = Blueprint('users', __name__)
+usuarios_bp = Blueprint('usuario', __name__)
 
-@users_bp.route('usuarios/users', methods=['POST'])
-def create_user():
-    # Obtener los datos del usuario de la solicitud
-    data = request.json
-    username = data.get('username')
-    email = data.get('email')
-    password = data.get('password')
-    first_name = data.get('first_name')
-    last_name = data.get('last_name')
-    phone = data.get('phone')
-    birthdate = data.get('birthdate')
-    gender = data.get('gender')
-    country = data.get('country')
-
-    # Verificar que se proporcionen todos los campos necesarios
-    if not (username and email and password and first_name and last_name):
-        return jsonify({'error': 'Todos los campos obligatorios deben ser proporcionados'}), 400
-
-    # Aquí insertarías al nuevo usuario en la tabla 'users'
-    conn = connect()  # Suponiendo que tienes una función 'connect()' que te devuelve una conexión a la base de datos
+@usuarios_bp.route('/usuarios/get_user', methods = ['GET'])
+def get_users():
+    conn = connect()
     cursor = conn.cursor()
-    cursor.execute("""
-        INSERT INTO public.users(username, email, password, first_name, last_name, phone, birthdate, gender, country)
-        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-    """, (username, email, password, first_name, last_name, phone, birthdate, gender, country))
-    conn.commit()
-    conn.close()
-
-    return jsonify({'message': 'Usuario agregado correctamente'}), 201
+    cursor.execute("SELECT * FROM public.users;")
+    usuarios_data = cursor.fetchall()
+    cursor.close()
+    usuarios_list = []
+    for usuario in usuarios_data:
+        usuario_dict =  {
+            'id': usuario[0],
+            'username': usuario[1],
+            'email' : usuario[2],
+            'password': usuario[3],
+            'first_name': usuario [4],
+            'last_name': usuario[5],
+            'phone': usuario[6],
+            'birthdate': usuario[7],
+            'gender': usuario[8],
+            'country': usuario[9]
+        }
+        usuarios_list.append(usuario_dict)
+    return jsonify(usuarios_list)
+        
+        
+        
+        # username, email, password, first_name, last_name, phone, birthdate, gender, country
