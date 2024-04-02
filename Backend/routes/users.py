1a2226dcf632c18c8350c31df7828544ebf25e0d
Paulo Ramirez <pa.ramirezciani13@gmail.com>
2024-04-02 11:07:33 -0300

    script nuevo db

--- Backend/routes/users.py
+++ Backend/routes/users.py
@@ -6,42 +6,40 @@ from flask_bcrypt import generate_password_hash
 usuarios_bp = Blueprint('usuario', __name__)
 
 #POST PARA CREAR USUARIO CON LA PASSWORD ENCRIPTADA
-@usuarios_bp.route('/usuarios/register', methods=['POST'])
+@usuarios_bp.route('/usuarios/clientes', methods=['POST'])
 def register():
     try:
         data = request.json
-        username = data['username']
-        email = data['email']
+        user_name = data['user_name']
         password = data['password']
         first_name = data['first_name']
-        last_name  = data['last_name']
-        phone  = data['phone']
-        birthdate  = data['birthdate']
-        gender  = data['gender']
-        country  = data['country']
-        
+        last_name = data['last_name']
+        email = data['email']
+        phone = data['phone']
+        birth_date = data['birth_date']
+        address = data['address']
+
         #encriptar la password
         hashed_password = generate_password_hash(password)
-        
+
         conn = connect()
         cursor = conn.cursor()
         cursor.execute("""
-            INSERT INTO public.users(
-                username, email, password, first_name, last_name,
-                phone, birthdate, gender, country
+            INSERT INTO public.cliente(
+                user_name, password, first_name, last_name, mail, address,
+                phone, birth_date
             )
-            VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-        """, (
-            username, email, hashed_password, first_name, last_name,
-            phone, birthdate, gender, country
-        ))
+            VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
+        """, 
+        (user_name, hashed_password, first_name, last_name, email, address,
+         phone, birth_date)
+        )
         conn.commit()
         conn.close()
-    
+
         return jsonify({'message': 'Usuario registrado correctamente'}), 201
     except Exception as e:
         return jsonify({'error': str(e)}), 500
-        
 
 #GET PARA OBTENER USUARIOS (LISTA)
 @usuarios_bp.route('/usuarios/get_user', methods = ['GET'])
